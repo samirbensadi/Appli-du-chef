2c7348092b859ee7b1d7781248b76d68a00ac1f9
Nabil Ghedjati <nabil.ghedjati@gmail.com>
2017-01-24 14:56:16 +0100

    modif test php

--- php/test.php
+++ php/test.php
@@ -5,10 +5,12 @@
  * Date: 24/01/17
  * Time: 10:34
  */
+if (!empty($_GET['mdp'])) {
+    $mdp = $_GET['mdp'];
 
-$mdp = "afpa";
+    $mdp_hash = password_hash($mdp, PASSWORD_BCRYPT); // hachage du mdp
+    echo $mdp_hash;
 
-$mdp_hash = password_hash($mdp, PASSWORD_BCRYPT); // hachage du mdp
-echo $mdp_hash;
+}
 
 ?>
