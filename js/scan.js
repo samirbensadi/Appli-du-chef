59e50f76ec8462ff80551c43a1516ecd3516a22d
Nabil Ghedjati <nabil.ghedjati@gmail.com>
2017-01-24 16:44:47 +0100

    essai barcode scanner

--- js/scan.js
+++ js/scan.js
@@ -1,11 +1,35 @@
 $(document).on('pagecreate', "#scan", function () {
-    QRScanner.scan(function (err, txt) {
-        if (err)  {
-            alert(err);
-        } else {
-            alert(txt);
-        }
-    });
+    // QRScanner.scan(function (err, txt) {
+    //     if (err)  {
+    //         alert(err);
+    //     } else {
+    //         alert(txt);
+    //     }
+    // });
+    //
+    // QRScanner.show();
 
-    QRScanner.show();
+
+
+    function scan()
+    {
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                if(!result.cancelled)
+                {
+                    alert("Scan");
+                    if(result.format == "QR_CODE")
+                    {
+                        alert("Done");
+
+                    }
+
+                }
+            },
+            function (error) {
+                alert("Scanning failed: " + error);
+            }
+        );
+    }
+
 });
